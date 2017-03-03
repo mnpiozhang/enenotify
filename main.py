87cbd0c23065cccec9146fbdf8d5b691d9b2230d
cloudhzl <cloudhzl@163.com>
2017-03-03 15:38:54 +0800

    修复连续发邮件的bug

--- main.py
+++ main.py
@@ -4,22 +4,37 @@ import function
 import time
 from common import  EmailNotify
 from common import  GetConfigList
+import threading
+
 lst = GetConfigList('stock_number')
-if __name__=='__main__':
+
+
+def get_ene(stock_number):
     while True:
-        for i in lst:
-            realtimePrice = function.GetRealTimePrice(i)
-            enelst = function.CalculateRealTimeENE(i, 10, 11, 9)
-            upperprice = enelst[0]
-            lowerprice = enelst[1]
-            middleprice = enelst[2]
-            print realtimePrice,upperprice,middleprice,lowerprice
-            if realtimePrice > upperprice:
-                message = u'打到上轨了  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)  
-                EmailNotify(message,message)
-            elif realtimePrice < lowerprice:
-                message = u'跌到下轨了  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)
-                EmailNotify(message,message)
-            else:
-                print  u'还在轨道通道内  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)
+        realtimePrice = function.GetRealTimePrice(stock_number)
+        enelst = function.CalculateRealTimeENE(stock_number, 10, 11, 9)
+        upperprice = enelst[0]
+        lowerprice = enelst[1]
+        middleprice = enelst[2]
+        print realtimePrice,upperprice,middleprice,lowerprice
+        if realtimePrice > upperprice:
+            message = u'打到上轨了  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)  
+            EmailNotify(message,message)
             time.sleep(1800)
+        elif realtimePrice < lowerprice:
+            message = u'跌到下轨了  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)
+            EmailNotify(message,message)
+            time.sleep(1800)
+        else:
+            print  u'还在轨道通道内  %s 现价 %s 上轨 %s 中轨%s 下轨%s'%(i,realtimePrice,upperprice,middleprice,lowerprice)
+            time.sleep(10)
+
+if __name__=='__main__':
+    threadlst = []
+    for i in lst:
+        t = threading.Thread(target=get_ene,args=(i,))
+        t.start()
+        threadlst.append(t)
+    print "ene calculate start"
+    for i in threadlst:
+        i.join()
